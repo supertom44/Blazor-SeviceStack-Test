9e97a412dc70fb44ca98973c1ef583f21b49f194
Tom Horan <t.v.horan@gmail.com>
2022-04-24 13:30:22 +0100

    Remove hsts

--- test/Program.cs
+++ test/Program.cs
@@ -15,8 +15,7 @@ else
 {
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-    app.UseHttpsRedirection();
+    //app.UseHsts();
 }
 app.UseHttpsRedirection();
 app.UseBlazorFrameworkFiles();
