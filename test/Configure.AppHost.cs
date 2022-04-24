24384335d5629acdb80306faa186af3029923295
Tom Horan <t.v.horan@gmail.com>
2022-04-24 10:55:04 +0100

    Removing cors

--- test/Configure.AppHost.cs
+++ test/Configure.AppHost.cs
@@ -14,13 +14,6 @@ public class AppHost : AppHostBase, IHostingStartup
     {
         SetConfig(new HostConfig {
         });
-
-        Plugins.Add(new CorsFeature(allowedHeaders: "Content-Type,Authorization",
-            allowOriginWhitelist: new[]{
-            "http://localhost:5000",
-            "https://localhost:5001",
-            "https://" + Environment.GetEnvironmentVariable("DEPLOY_CDN")
-        }, allowCredentials: true));
     }
 
     public void Configure(IWebHostBuilder builder) => builder
